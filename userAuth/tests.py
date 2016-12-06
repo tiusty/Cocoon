cbf0e5d2c7d929a6bd1977aeae0d40a340c911a8
Tiusty <awagud12@gmail.com>
2016-12-05 21:55:13 -0500

    Added tests to userAuth page

--- userAuth/tests.py
+++ userAuth/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from userAuth.forms import RegisterForm,LoginForm
+
+
+class RegisterFormTest(TestCase):
+    def test_form_correct_input(self):
+        form_data = {
+            'first_name': 'Alex',
+            'last_name': 'Agudelo',
+            'email': 'email@test.com',
+            'password1': 'sometestPassword',
+            'password2': 'sometestPassword',
+        }
+        form = RegisterForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_form_different_passwords(self):
+        form_data = {
+            'first_name': 'Alex',
+            'last_name': 'Agudelo',
+            'email': 'email@test.com',
+            'password1': 'sometestPassword',
+            'password2': 'sometestPasswordDifferent',
+        }
+        form = RegisterForm(data=form_data)
+        self.assertFalse(form.is_valid)
