c1b16113f74432455a9a7b2399669e8c1144dac7
Tiusty <awagud12@gmail.com>
2017-01-18 00:03:10 -0500

    Adding remote ip address

--- Unicorn/settings.py
+++ Unicorn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#o3k!ghm%yfnscj1bam=p*ml=$ct9b0j00-bkv@-%%=jk&54g('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.14.25.81']
 
 
 # Application definition
