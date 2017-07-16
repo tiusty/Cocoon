c70f30eb3c6a9323cf709e5f4904bd4bd5435f12
seanrayment <rayment.code@gmail.com>
2017-07-16 19:51:19 -0400

    beginnings of expanded tile view

--- survey/static/survey/expandTile.js
+++ survey/static/survey/expandTile.js
@@ -1,3 +1,8 @@
-$('.tile').on('click', ':not(.glyphicon)',  function (e) {
-    $(this).parent().addClass('tile-expanded');
+$('.tile').bind('click', ':not(.glyphicon)',  function (e) {
+    e.stop
+    $(this).removeClass('tile');
+    $(this).addClass('tile-expanded');
+    $(this).empty()
+    $('.tile').hide();
+
 })
