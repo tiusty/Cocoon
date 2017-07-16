00746a224901dd1c62590daf3c189f2cdce290e5
seanrayment <rayment.code@gmail.com>
2017-07-16 13:35:24 -0400

    changed tile layout and hover effects

--- survey/static/survey/tileInteraction.js
+++ survey/static/survey/tileInteraction.js
@@ -3,16 +3,40 @@
  */
 
 $(document).ready(function () {
+
+    // sets the color of the score based on the value
+    $('.scoreText').each(function () {
+
+        var scoreValue = parseInt($(this).text())
+        console.log(scoreValue)
+
+        if (scoreValue > 92) {
+            $(this).addClass('great-match');
+        } else if (scoreValue > 86 ) {
+            $(this).addClass('good-match');
+        } else if (scoreValue > 76) {
+            $(this).addClass('acceptable-match');
+        } else if (scoreValue > 66) {
+            $(this).addClass('bad-match');
+        } else {
+            $(this).addClass('terrible-match');
+        }
+    });
+
     // oversize the contents of the tile when hovered to suggest that
     // it can be expanded when clicked
     $('.tile').mouseover(function(){
+        $(this).find('#thumbnailItem1').addClass('thumbnailItem1-hover');
+        $(this).find('.thumbnailItem').addClass('thumbnailItem-hover');
         $(this).find('.thumbnailImage').addClass('thumbnailImage-hover');
         $(this).find('.scoreText').addClass('scoreText-hover');
         $(this).find('.infoBit').addClass('infoBit-hover');
     });
 
     $('.tile').mouseleave(function () {
-         $('.thumbnailImage').removeClass('thumbnailImage-hover');
+        $(this).find('#thumbnailItem1').removeClass('thumbnailItem1-hover');
+        $(this).find('.thumbnailItem').removeClass('thumbnailItem-hover');
+        $('.thumbnailImage').removeClass('thumbnailImage-hover');
         $('.scoreText').removeClass('scoreText-hover');
         $('.infoBit').removeClass('infoBit-hover');
     });
