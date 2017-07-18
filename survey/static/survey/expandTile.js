a052173cfa1b9cefe35d7204fdd44d950b634fc3
seanrayment <rayment.code@gmail.com>
2017-07-18 07:17:42 -0400

    expanding tile work

--- survey/static/survey/expandTile.js
+++ survey/static/survey/expandTile.js
@@ -1,7 +1,19 @@
 $('.tile').bind('click', ':not(.glyphicon)',  function () {
-    $(this).removeClass('tile');
+    //$(this).removeClass('tile');
     $(this).addClass('tile-expanded');
+    $(this).removeClass('tile');
     $(this).children().hide()
-    //$('.tile').hide();
+    $('.tile').hide();
 
 })
+
+
+
+/********************
+
+ Other  options to achieve a similar effect
+
+ flexbox, animate flex-grow, flex-basis
+ Jquery slideDown entrance animation\
+
+ ********************/
