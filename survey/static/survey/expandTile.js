795b6ef97d1ef4d771cb7b1809c22030ac62f142
seanrayment <rayment.code@gmail.com>
2017-08-08 17:52:15 -0400

    tile overhaul. frontend now renders favorited and unfavorited tiles in real time before page reload

--- survey/static/survey/expandTile.js
+++ survey/static/survey/expandTile.js
@@ -1,17 +1,63 @@
-clickBind = $('.tile').bind('click', function () {
+clickBind = $('.tile').on('click', function () {
 
     if ($(this).hasClass('bound')) {
-         $(this).removeClass('tile');
-    $(this).addClass('tile-expanded');
-    $(this).children().hide();
-    $(this).animate({"height": "65vh"}, 200, function () {
-        $(this).siblings('.tile').slideUp(200);
+        var isFavorite = $(this).find('.heartGlyph').hasClass("glyphicon-heart");
+        console.log(isFavorite);
+        $(this).removeClass('tile');
+        $(this).addClass('tile-expanded');
+        $(this).children().hide();
+        $(this).animate({"height": "65vh"}, 200, function () {
+            $(this).siblings('.tile').slideUp(200);
+        });
+        /*
+         * Adds the unloaded templated html to the page
+         */
+        $(this).append($(this).children('#expanded-tile-contents').html());
+
+        /*
+         *  Adds the event listener for removing tile if unfavorited within the expanded tile
+         *
+         */
+
+        $(".expanded-glyph").click(function () {
+
+        if ($(this).hasClass('glyphicon-heart')) {
+            console.log("favorited!");
+
+            if ($('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').length) {
+                 $('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').fadeOut();
+            } else {
+                $(this).parents('.tile-expanded').addClass("remove");
+            }
+
+
+        } else {
+            console.log("not favorited");
+
+            if ($('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').length) {
+                $('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').fadeIn();
+            } else {
+
+                console.log("ADDING HTML AGAIN");
+                $('.favoriteTiles').append($(this).parents('.tile').clone(true, true));
+
+            }
+        }
+
     });
-    /*
-     * Adds the unloaded templated html to the page
-     */
-    $(this).append($(this).children('#expanded-tile-contents').html());
-    $(this).removeClass('bound');
+
+
+        /*
+           Updates the heart glyph when tile expanded
+         */
+        if (isFavorite) {
+            $(".expanded-tile-container").find(".expanded-glyph").removeClass("glyphicon-heart-empty").addClass('glyphicon-heart');
+        } else {
+            console.log("not here");
+            $(".expanded-tile-container").find(".expanded-glyph").removeClass("glyphicon-heart").addClass('glyphicon-heart-empty');
+        }
+
+        $(this).removeClass('bound');
     }
 });
 
@@ -19,9 +65,7 @@ function minimize(clickedElement) {
 
     $(clickedElement).closest('.expanded-tile-container').hide();
 
-    console.log($(clickedElement).parents());
-
-     $(clickedElement).parents('.tile-expanded').animate({"height": "115px"}, 200, function() {
+    $(clickedElement).parents('.tile-expanded').animate({"height": "115px"}, 200, function () {
 
         $(clickedElement).parents('.tile-expanded').addClass('tile');
         $(clickedElement).parents('.tile').siblings('.tile').slideDown(200);
@@ -31,7 +75,11 @@ function minimize(clickedElement) {
 
         $(clickedElement).closest('.expanded-tile-container').remove();
 
-     })
+        $('.remove').fadeOut(300);
+
+
+
+    })
 }
 
 $('.glyphicon-heart, .glyphicon-heart-empty').click(function (e) {
---
+++ survey/static/survey/unfavorite.js
@@ -0,0 +1,55 @@
+/*
+
+ This script is for rendering and unrendering tiles on the frontend before
+ a page refresh occurs in order to make the process more seamless.
+
+ If the tile is not in the actual html due to a page refresh, the DOM is
+ augmented. Otherwise, it is just redisplayed block.
+
+ Problems: Unfavoriting from expanded tiles is not implemented.
+
+ Visit list tiles unfavorited do not update glyphs on expanding action after page reload
+
+ */
+
+$(document).ready(function () {
+
+    $(".heartGlyph").on('click', function () {
+
+        if ($(this).hasClass('glyphicon-heart')) {
+            console.log("favorited!");
+            $('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').fadeOut();
+        } else {
+            console.log("not favorited");
+
+            if ($('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').length) {
+                $('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').fadeIn();
+            } else {
+                console.log("ADDING HTML AGAIN");
+                $('.favoriteTiles').append($(this).parents('.tile').clone(true, true));
+            }
+        }
+    });
+
+    $(".expanded-glyph").click(function () {
+
+        console.log("CLICKED MEEE");
+
+        if ($(this).hasClass('glyphicon-heart')) {
+            console.log("favorited!");
+            $('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').fadeOut();
+        } else {
+            console.log("not favorited");
+
+            if ($('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').length) {
+                $('.favoriteTiles > .tile > .scoreItem > #' + $(this).prop('id')).parents('.tile').fadeIn();
+            } else {
+                console.log("ADDING HTML AGAIN");
+                $('.favoriteTiles').append($(this).parents('.tile').clone(true, true));
+            }
+
+        }
+
+    });
+
+})
