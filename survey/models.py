c1e641c87db6f3f033d71dfaf5510adf8f383a73
Tiusty <awagud12@gmail.com>
2017-12-24 00:08:06 -0500

    Updated RentingDestination model

--- survey/models.py
+++ survey/models.py
@@ -11,7 +11,6 @@ from Unicorn.settings.Global_Config import MAX_NUM_BATHROOMS, DEFAULT_RENT_SURVE
 
 # Import other models
 from userAuth.models import UserProfile
-from houseDatabase.models import RentDatabase
 
 
 class CommutePrecision(Enum):
@@ -59,6 +58,7 @@ class InitialSurveyModel(models.Model):
     _name = models.CharField(max_length=200, default=DEFAULT_RENT_SURVEY_NAME)
     _survey_type = models.IntegerField(default=-1)
     _created = models.DateField(auto_now_add=True)
+    _user_profile = models.ForeignKey(UserProfile)
 
     @property
     def name(self):
@@ -71,6 +71,10 @@ class InitialSurveyModel(models.Model):
     @property
     def created(self):
         return self._created
+
+    @property
+    def user_profile(self):
+        return self._user_profile
 
     class Meta:
         abstract = True
@@ -111,6 +115,23 @@ class HomeInformationModel(models.Model):
     def home_type(self):
         return self._home_type
 
+    @property
+    def home_types(self):
+        home_type_set = self.home_type.all()
+        if home_type_set.count() == 0:
+            return "Not set"
+        else:
+            type_output = ""
+            counter = 0
+            for homeType in home_type_set:
+                if counter == 0:
+                    type_output = str(homeType)
+                    counter += 1
+                else:
+                    type_output = str(homeType) + ", " + type_output
+
+        return type_output
+
     class Meta:
         abstract = True
 
@@ -148,6 +169,25 @@ class CommuteInformationModel(models.Model):
     def commute_type(self):
         return self._commute_type
 
+    @property
+    def commute_range(self):
+        """
+        Return the commute range as a string
+        :return: String -> Commute range
+        """
+        if self.max_commute > 60:
+            max_output = str(math.floor(self.max_commute / 60)) + " hours " + str(self.max_commute % 60) \
+                         + " Minutes"
+        else:
+            max_output = str(self.max_commute) + " Minutes"
+        if self.min_commute > 60:
+            min_output = str(math.floor(self.min_commute / 60)) + " hours " + str(self.min_commute % 60) \
+                         + " Minutes"
+        else:
+            min_output = str(self.min_commute) + " Minutes"
+
+        return min_output + " - " + max_output
+
     class Meta:
         abstract = True
 
@@ -171,6 +211,15 @@ class PriceInformationModel(models.Model):
     @property
     def price_weight(self):
         return self._price_weight
+
+    @property
+    def price_range(self):
+        """
+        Returns the price range
+        :return: String -> Price range
+        """
+        price_output = "$" + str(self.min_price) + " - $" + str(self.max_price)
+        return price_output
 
     class Meta:
         abstract = True
@@ -250,72 +299,24 @@ class ExteriorAmenitiesModel(models.Model):
         abstract = True
 
 
-class RentingSurveyModel(ExteriorAmenitiesModel, InteriorAmenitiesModel, PriceInformationModel, CommuteInformationModel, HomeInformationModel, InitialSurveyModel):
+class RentingSurveyModel(ExteriorAmenitiesModel, InteriorAmenitiesModel, PriceInformationModel, CommuteInformationModel,
+                         HomeInformationModel, InitialSurveyModel):
     """
     Renting Survey Model is the model for storing data from the renting survey model.
-    It takes the Initial Survey model as an input which is data that is true for all surveys
     The user may take multiple surveys and it is linked to their User Profile
 
     Default name is stored unless the User changes it. Every time a survey is created the past
     default name is deleted to allow for the new one. Therefore, there is always a history
     of one survey
     """
-    user_profile = models.ForeignKey(UserProfile)
-
-    def get_short_name(self):
-        user_short_name = self.user_profile.user.get_short_name()
-        survey_name = self.name()
-        output = user_short_name + ": " + survey_name
-        return output
 
     def __str__(self):
         user_short_name = self.user_profile.user.get_short_name()
-        survey_name = self.name()
+        survey_name = self.name
         output = user_short_name + ": " + survey_name
         return output
 
-    def get_cost_range(self):
-        if self.max_price() == 0:
-            return "Not set"
-        else:
-            price_output = "$" + str(self.min_price()) + " - $" + str(self.max_price())
-            return price_output
 
-    def get_commute_range(self):
-        if self.max_commute() == 0:
-            return "Not Set"
-        else:
-            if self.max_commute() > 60:
-                max_output = str(math.floor(self.max_commute() / 60)) + " hours " + str(self.max_commute() % 60) \
-                             + " Minutes"
-            else:
-                max_output = str(self.max_commute()) + " Minutes"
-            if self.min_commute() > 60:
-                min_output = str(math.floor(self.min_commute() / 60)) + " hours " + str(self.min_commute() % 60) \
-                             + " Minutes"
-            else:
-                min_output = str(self.min_commute()) + " Minutes"
-
-        return min_output + " - " + max_output
-
-    def get_home_types(self):
-        home_type_set = self.home_type.all()
-        if home_type_set.count() == 0:
-            return "Not set"
-        else:
-            type_output = ""
-            counter = 0
-            for homeType in home_type_set:
-                if counter == 0:
-                    type_output = str(homeType)
-                    counter += 1
-                else:
-                    type_output = str(homeType) + ", " + type_output
-
-        return type_output
-
-
-# Stores the destination address since there can be multiple
 class Destinations(models.Model):
     street_address = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
