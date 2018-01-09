ca78eb4189a57a0c2383e95d06218e8057ccafea
seanrayment <sean_rayment@brown.edu>
2018-01-09 09:16:09 -0500

    working on mls testing

--- houseDatabase/mlspin_tests/test_pull_mls.py
+++ houseDatabase/mlspin_tests/test_pull_mls.py
@@ -35,6 +35,7 @@ class TestPullMlspin(TestCase):
         home2 = RentDatabaseModel.objects.get(pk=2) # 296 Marlborough St.
         home3 = RentDatabaseModel.objects.get(pk=3) # 784 Tremont St.
 
+        # asserts for the first home
         self.assertEqual(home1.street_address_home, "12 Mount Vernon St.")
         self.assertEqual(home1.city_home, "Boston")
         self.assertEqual(home1.zip_code_home, "02129")
@@ -46,7 +47,41 @@ class TestPullMlspin(TestCase):
         self.assertEqual(home1.num_bedrooms_home, 2)
         self.assertEqual(home1.num_bathrooms_home, 1)
         self.assertEqual(home1.bath_home, True)
-        self.assertEqual(home1.listing_number_home, "71811023")
+        self.assertEqual(home1.listing_number_home, 71811023)
         self.assertEqual(home1.listing_agent_home, "BB808729")
         self.assertEqual(home1.listing_provider_home, "MLSPIN")
         self.assertEqual(home1.listing_office_home, "AN1037")
+
+        # asserts for the second home
+        self.assertEqual(home2.street_address_home, "296 Marlborough St")
+        self.assertEqual(home2.city_home, "Boston")
+        self.assertEqual(home2.zip_code_home, "02114")
+        self.assertEqual(home2.price_home, 2850)
+        self.assertEqual(home2.home_type_home, self.home_type)
+        self.assertEqual(str(home2.latitude_home), "42.351246")
+        self.assertEqual(str(home2.longitude_home), "-71.083828")
+        self.assertEqual(home2.state_home, "MA")
+        self.assertEqual(home2.num_bedrooms_home, 1)
+        self.assertEqual(home2.num_bathrooms_home, 1)
+        self.assertEqual(home2.bath_home, True)
+        self.assertEqual(home2.listing_number_home, 71738853)
+        self.assertEqual(home2.listing_agent_home, "BB808729")
+        self.assertEqual(home2.listing_provider_home, "MLSPIN")
+        self.assertEqual(home2.listing_office_home, "AN1037")
+
+        # asserts for the third home
+        self.assertEqual(home3.street_address_home, "784 Tremont Street")
+        self.assertEqual(home3.city_home, "Boston")
+        self.assertEqual(home3.zip_code_home, "02118")
+        self.assertEqual(home3.price_home, 3460)
+        self.assertEqual(home3.home_type_home, self.home_type)
+        self.assertEqual(str(home3.latitude_home), "42.338892")
+        self.assertEqual(str(home3.longitude_home), "-71.080790")
+        self.assertEqual(home3.state_home, "MA")
+        self.assertEqual(home3.num_bedrooms_home, 1)
+        self.assertEqual(home3.num_bathrooms_home, 1)
+        self.assertEqual(home3.bath_home, True)
+        self.assertEqual(home3.listing_number_home, 72080819)
+        self.assertEqual(home3.listing_agent_home, "BB808729")
+        self.assertEqual(home3.listing_provider_home, "MLSPIN")
+        self.assertEqual(home3.listing_office_home, "AN1037")
