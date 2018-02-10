39c7871a7b1b6669b16e25fb0ff3983378b2a35e
Alex Agudelo <awagud12@gmail.com>
2018-02-10 09:41:48 -0500

    Fix importing module

--- houseDatabase/admin.py
+++ houseDatabase/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import RentDatabaseModel, ZipCodeDictionaryParentModel, ZipCodeDictionaryChildModel, HousePhotosModel, \
-    HomeTypeModel, CommuteTypeModel, DatabaseManagementModel
+    HomeTypeModel, CommuteTypeModel, MlsManagementModel
 
 
 class HousePhotoUrlInLine(admin.StackedInline):
