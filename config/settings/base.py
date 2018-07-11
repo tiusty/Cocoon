4717622baa373e2edbf533d81ffffb3ff5603010
Alex Agudelo <awagud12@gmail.com>
2018-07-11 17:39:39 +0000

    Adds ability to connect to s3-bucket

--- config/settings/base.py
+++ config/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'cocoon.survey',
     'cocoon.houseDatabase',
     'cocoon.commutes',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "../cocoon/media/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "../cocoon/static"),
 ]
+
+AWS_STORAGE_BUCKET_NAME = 'bostoncocoon-assets'
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_ACCESS_KEY_ID = 'AKIAIL5BFKNQ6GZPNDWQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'config.settings.storage_backends.StaticStorage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION)
+
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+DEFAULT_FILE_STORAGE = 'config.settings.storage_backends.PublicMediaStorage'
---
+++ config/settings/storage_backends.py
@@ -0,0 +1,12 @@
+# Third party imports
+from storages.backends.s3boto3 import S3Boto3Storage
+
+# Settings import
+from config.settings.base import AWS_STATIC_LOCATION, AWS_PUBLIC_MEDIA_LOCATION
+
+class StaticStorage(S3Boto3Storage):
+    location = AWS_STATIC_LOCATION
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = AWS_PUBLIC_MEDIA_LOCATION
+    file_overwrite = False
