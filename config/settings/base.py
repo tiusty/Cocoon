155ac70a900f5e10c288d7339b709a4765baf481
Tiusty <awagud12@gmail.com>
2018-08-24 13:14:35 -0400

    [issue-120] The password reset now works with sending an actual email!

--- config/settings/base.py
+++ config/settings/base.py
@@ -123,4 +123,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "../cocoon/static"),
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'devteam@bostoncocoon.com'
+EMAIL_HOST_PASSWORD = 'projectunicorn2018'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'TestSite Team <noreply@example.com>'
