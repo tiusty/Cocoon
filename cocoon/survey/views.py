d6a4c789c6debae2edbbf3e86943294ac9f07085
Tiusty <awagud12@gmail.com>
2018-08-25 21:21:44 -0400

    [issue-122] Got rid of unncessary code. Make flow eaiser to read

--- cocoon/survey/views.py
+++ cocoon/survey/views.py
@@ -163,7 +163,7 @@ def run_rent_algorithm(survey, context):
 # Assumes the survey_slug will be passed by the URL if not, then it grabs the most recent survey.
 # If it can't find the most recent survey it redirects back to the survey
 @login_required
-def survey_result_rent(request, survey_slug="recent"):
+def survey_result_rent(request, survey_slug=""):
     """
     Survey result rent is the heart of the website where the survey is grabbed and the housing list is created
     Based on the results of the survey. This is specifically for the rent survey
@@ -181,36 +181,21 @@ def survey_result_rent(request, survey_slug="recent"):
     }
 
     user_profile = get_object_or_404(UserProfile, user=request.user)
-    # If no id is specified in the URL, then it attempts to load the recent survey
-    # The recent survey is the last survey to be created
-    if survey_slug == "recent":
-        # Try to retrieve the most recent survey, but if there are no surveys, then
-        # Redirect back to the homepage
+
+    # Tries to grab the survey. If the survey name was not passed in, then it grabs the most recent survey taken.
+    try:
+        survey = RentingSurveyModel.objects.filter(user_profile_survey=user_profile).get(slug=survey_slug)
+    # If the survey ID, does not exist/is not for that user, then return the most recent survey
+    except RentingSurveyModel.DoesNotExist:
         if RentingSurveyModel.objects.filter(user_profile_survey=user_profile).exists():
-            survey = RentingSurveyModel.objects.filter(user_profile_survey=user_profile) \
+            survey = RentingSurveyModel.objects.filter(user_profile_survey=user_profile)\
                 .order_by('created_survey').first()
+            messages.add_message(request, messages.WARNING, 'Could not find Survey, loading most recent survey')
             return HttpResponseRedirect(reverse('survey:rentSurveyResult',
                                                 kwargs={"survey_slug": survey.slug}))
         else:
-            messages.add_message(request, messages.ERROR, 'Could not find any surveys. Please take one!')
+            messages.add_message(request, messages.ERROR, 'Could not find Survey')
             return HttpResponseRedirect(reverse('homePage:index'))
-    else:
-        # If the user did not choose recent, then try to grab the survey by it's id
-        # If it can't find it or it is not associated with the user, just grab the
-        # Recent Survey. If that fails, then redirect back to the home page.
-        try:
-            survey = RentingSurveyModel.objects.filter(user_profile_survey=user_profile).get(slug=survey_slug)
-        # If the survey ID, does not exist/is not for that user, then return the most recent survey
-        except RentingSurveyModel.DoesNotExist:
-            if RentingSurveyModel.objects.filter(user_profile_survey=user_profile).exists():
-                survey = RentingSurveyModel.objects.filter(user_profile_survey=user_profile)\
-                    .order_by('created_survey').first()
-                messages.add_message(request, messages.WARNING, 'Could not find Survey, loading most recent survey')
-                return HttpResponseRedirect(reverse('survey:rentSurveyResult',
-                                                    kwargs={"survey_slug": survey.slug}))
-            else:
-                messages.add_message(request, messages.ERROR, 'Could not find Survey')
-                return HttpResponseRedirect(reverse('homePage:index'))
 
     # Populate form with stored data
     form = RentSurveyFormMini(instance=survey)
