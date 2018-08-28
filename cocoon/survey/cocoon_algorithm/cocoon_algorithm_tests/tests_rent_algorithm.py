dab462d88a14ce0f2daea78579febefbf22dfce3
Tiusty <awagud12@gmail.com>
2018-08-28 12:45:13 -0400

    [issue-127] Added tests for zip_code_approximation function

--- cocoon/survey/cocoon_algorithm/cocoon_algorithm_tests/tests_rent_algorithm.py
+++ cocoon/survey/cocoon_algorithm/cocoon_algorithm_tests/tests_rent_algorithm.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 # Import external models
 from cocoon.houseDatabase.models import RentDatabaseModel, HomeTypeModel, MlsManagementModel
 from cocoon.commutes.models import ZipCodeBase, CommuteType
+from cocoon.commutes.constants import ZIP_CODE_TIMEDELTA_VALUE
 # Import survey python modules
 from cocoon.survey.cocoon_algorithm.rent_algorithm import RentAlgorithm
 from cocoon.survey.home_data.home_score import HomeScore
@@ -1090,13 +1091,108 @@ class TestRetrieveApproximateCommutes(TestCase):
 
     @staticmethod
     def create_zip_code_dictionary_child(parent_zip_code_dictionary, zip_code, commute_time,
-                                         commute_distance, commute_type):
+                                         commute_distance, commute_type, last_updated=timezone.now()):
         parent_zip_code_dictionary.zipcodechild_set.create(
             zip_code=zip_code,
             commute_time_seconds=commute_time,
             commute_distance_meters=commute_distance,
             commute_type=commute_type,
+            last_date_updated=last_updated,
         )
+
+    def test_zip_code_approximation_combo_exists(self):
+        """
+        Tests that if the zip_combo exists then it will extract it from the zip-code database and use the values
+        """
+        # Arrange
+        survey = self.create_survey(self.user.userProfile)
+        destination = self.create_destination(survey, self.commute_type, street_address="100 Main Street")
+        zip_code = '02476'
+        home = self.create_home(self.home_type)
+        commute_distance = 100
+        commute_time_seconds = 376
+
+        # Create the zip-code dictionary
+        parent_zip_code = self.create_zip_code_dictionary(zip_code)
+        self.create_zip_code_dictionary_child(parent_zip_code, destination.zip_code, commute_time_seconds,
+                                              commute_distance, self.commute_type)
+
+        # Act
+        result = home.zip_code_approximation(zip_code, destination)
+
+        # Assert
+        self.assertTrue(result)
+        # Convert to minutes because that is what is returned
+        self.assertEqual(home.approx_commute_times, {destination: commute_time_seconds / 60})
+
+    def test_zip_code_approximation_child_does_not_exist(self):
+        """
+        Tests that if the parent zip code exists but not the child, then the function will return false
+            and the home will not be added to the list of commute times
+        """
+        # Arrange
+        survey = self.create_survey(self.user.userProfile)
+        destination = self.create_destination(survey, self.commute_type, street_address="100 Main Street")
+        zip_code = '02476'
+        home = self.create_home(self.home_type)
+
+        # Create the zip-code dictionary
+        self.create_zip_code_dictionary(zip_code)
+
+        # Act
+        result = home.zip_code_approximation(zip_code, destination)
+
+        # Assert
+        self.assertFalse(result)
+        # Convert to minutes because that is what is returned
+        self.assertEqual(home.approx_commute_times, {})
+
+    def test_zip_code_approximation_neither_exist(self):
+        """
+        Tests that if the parent/child zip code approximation doesn't exist then the function will return false and
+            the commute is not added to the approx_commute_times
+        """
+        # Arrange
+        survey = self.create_survey(self.user.userProfile)
+        destination = self.create_destination(survey, self.commute_type, street_address="100 Main Street")
+        zip_code = '02476'
+        home = self.create_home(self.home_type)
+
+        # Act
+        result = home.zip_code_approximation(zip_code, destination)
+
+        # Assert
+        self.assertFalse(result)
+        # Convert to minutes because that is what is returned
+        self.assertEqual(home.approx_commute_times, {})
+
+    def test_zip_code_approximation_exit_not_valid(self):
+        """
+        Tests that if the parent/child zip code pair exists but are out of date then the commute is not added
+            and the function returns false
+        """
+        # Arrange
+        survey = self.create_survey(self.user.userProfile)
+        destination = self.create_destination(survey, self.commute_type, street_address="100 Main Street")
+        zip_code = '02476'
+        home = self.create_home(self.home_type)
+        commute_distance = 100
+        commute_time_seconds = 376
+
+        # Create the zip-code dictionary
+        parent_zip_code = self.create_zip_code_dictionary(zip_code)
+        self.create_zip_code_dictionary_child(parent_zip_code, destination.zip_code, commute_time_seconds,
+                                              commute_distance, self.commute_type,
+                                              last_updated=timezone.now() -
+                                              timezone.timedelta(days=ZIP_CODE_TIMEDELTA_VALUE + 1))
+
+        # Act
+        result = home.zip_code_approximation(zip_code, destination)
+
+        # Assert
+        self.assertFalse(result)
+        # Convert to minutes because that is what is returned
+        self.assertEqual(home.approx_commute_times, {})
 
     def test_retrieve_approx_commutes_in_database_one_home_one_destination(self):
         # Arrange
