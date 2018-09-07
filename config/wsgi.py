a61b5ed054b00316771983fb06b1b7542d45b303
Tiusty <awagud12@gmail.com>
2018-09-07 11:01:35 -0400

    [issue-159] Adds the ability to load the settings file via the secrets file

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,26 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import json
 
 from django.core.wsgi import get_wsgi_application
+from django.core.exceptions import ImproperlyConfigured
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.staging")
+# Opens the secrets file and loads the values
+with open(os.path.expanduser("~") + '/work/Cocoon/config/settings/secrets.json') as f:
+        secrets = json.loads(f.read())
+
+
+# Function loads a value from the secrets file
+def get_secret(setting, secrets=secrets):
+        try:
+                return secrets[setting]
+        except KeyError:
+                        error_msg = 'Set the {0} environment variable'.format(setting)
+                        raise ImproperlyConfigured(error_msg)
+
+
+# Loads the settings file via the secrets file settings
+os.environ["DJANGO_SETTINGS_MODULE"] = get_secret('DJANGO_SETTINGS_MODULE')
 
 application = get_wsgi_application()
