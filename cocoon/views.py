5824b491459032917c35e49913b6281593d548a2
Tiusty <awagud12@gmail.com>
2018-09-07 19:00:01 -0400

    [issue-165] Made 404 error message much nicer

--- cocoon/views.py
+++ cocoon/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def error_404_view(request, exception):
     data = {"name": "ThePythonDjango.com"}
-    return render(request, 'error_pages/errot_404.html', data)
+    return render(request, 'error_pages/error_404.html', data)
