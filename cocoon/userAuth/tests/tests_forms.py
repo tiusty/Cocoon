26acef040f9c758c479e6d7f7a2b3996e9a1bd25
Tiusty <awagud12@gmail.com>
2018-09-09 22:22:08 -0400

    [issue-158] Fixed userAuth unittests

---
+++ cocoon/userAuth/tests/tests_forms.py
@@ -0,0 +1,244 @@
+from django.test import TestCase
+
+from cocoon.userAuth.forms import ApartmentHunterSignupForm, BrokerSignupForm, LoginUserForm
+
+# Import cocoon global config
+from cocoon.userAuth.constants import HUNTER_CREATION_KEY
+
+
+class TestApartmentHunterSingupForm(TestCase):
+
+    def tests_form_valid(self):
+        """
+        Tests that given the correct info the form will validate
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email@text.com'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword'
+        creation_key = HUNTER_CREATION_KEY
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = ApartmentHunterSignupForm(data=form_data)
+
+        # Assert
+        self.assertTrue(form.is_valid())
+
+    def test_form_wrong_key(self):
+        """
+        Tests thta given the wrong key the form doesn't validate
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email@text.com'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword'
+        creation_key = 'some_random_key'
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = ApartmentHunterSignupForm(data=form_data)
+
+        # Assert
+        self.assertFalse(form.is_valid())
+        self.assertEqual({'creation_key': ['Creation Key invaild']}, form.errors)
+
+    def test_form_not_matching_passwords(self):
+        """
+        Tests that given non-matching passwords, the form will not validate
+        :return:
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email@text.com'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword1'
+        creation_key = HUNTER_CREATION_KEY
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = ApartmentHunterSignupForm(data=form_data)
+
+        # Assert
+        self.assertFalse(form.is_valid())
+        self.assertEqual({'password2': ["The two password fields didn't match."]}, form.errors)
+
+    def test_form_email_not_valid(self):
+        """
+        Tests that if the email field is not a valid email, the form will not validate
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword'
+        creation_key = HUNTER_CREATION_KEY
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = ApartmentHunterSignupForm(data=form_data)
+
+        # Assert
+        self.assertFalse(form.is_valid())
+        self.assertEqual({'email': ['Enter a valid email address.']}, form.errors)
+
+
+class TestLoginUserForm(TestCase):
+
+    def tests_form_valid(self):
+        """
+        Tests that given the correct info the form will validate
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email@text.com'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword'
+        creation_key = BrokerSignupForm
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = BrokerSignupForm(data=form_data)
+
+        # Assert
+        self.assertTrue(form.is_valid())
+
+    def test_form_wrong_key(self):
+        """
+        Tests thta given the wrong key the form doesn't validate
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email@text.com'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword'
+        creation_key = 'some_random_key'
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = BrokerSignupForm(data=form_data)
+
+        # Assert
+        self.assertFalse(form.is_valid())
+        self.assertEqual({'creation_key': ['Creation Key invaild']}, form.errors)
+
+    def test_form_not_matching_passwords(self):
+        """
+        Tests that given non-matching passwords, the form will not validate
+        :return:
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email@text.com'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword1'
+        creation_key = BrokerSignupForm
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = BrokerSignupForm(data=form_data)
+
+        # Assert
+        self.assertFalse(form.is_valid())
+        self.assertEqual({'password2': ["The two password fields didn't match."]}, form.errors)
+
+    def test_form_email_not_valid(self):
+        """
+        Tests that if the email field is not a valid email, the form will not validate
+        """
+        # Arrange
+        first_name = 'Alex'
+        last_name = 'Agudelo'
+        username = 'email'
+        password1 = 'sometestPassword'
+        password2 = 'sometestPassword'
+        creation_key = BrokerSignupForm
+
+        # Create form data
+        form_data = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': username,
+            'password1': password1,
+            'password2': password2,
+            'creation_key': creation_key
+        }
+
+        # Act
+        form = BrokerSignupForm(data=form_data)
+
+        # Assert
+        self.assertFalse(form.is_valid())
+        self.assertEqual({'email': ['Enter a valid email address.']}, form.errors)
