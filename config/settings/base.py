65d9607d59000d3c9b6be3373158034798c87053
Tiusty <awagud12@gmail.com>
2018-09-27 22:43:42 -0400

    [issue-139]  Finished with the django rest framework tutorials

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
