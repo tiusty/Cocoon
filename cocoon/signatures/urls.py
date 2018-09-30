5cd4674f23b451d75f01182e7dee47fe07798620
Tiusty <awagud12@gmail.com>
2018-09-30 09:57:27 -0400

    [issue-139] Moved signature url to a name-space

--- cocoon/signatures/urls.py
+++ cocoon/signatures/urls.py
@@ -9,5 +9,5 @@ router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^signatures/', include(router.urls))
 ]
