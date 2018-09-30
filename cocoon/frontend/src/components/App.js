ce6f2c19dfaee170fd8334df7b7e12bcc72b38e4
Tiusty <awagud12@gmail.com>
2018-09-30 10:21:34 -0400

    [issue-139] Got the react to interface with the reset framework!!

--- cocoon/frontend/src/components/App.js
+++ cocoon/frontend/src/components/App.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Table from "./Table";
 const App = () => (
-  <DataProvider endpoint="api/lead/"
-                render={data => <Table data={data} />} />
+  <DataProvider endpoint="http://127.0.0.1:8000/signatures/snippets/"
+                render={data => <Table data={data.results} />} />
 );
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
