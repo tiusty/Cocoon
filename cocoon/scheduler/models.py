fde7a6f300b8b39a4d859f50c038b231ec75789f
Sean Rayment <sean_rayment@brown.edu>
2018-10-16 23:55:02 -0400

    Moved time foreign key to TimeModel

--- cocoon/scheduler/models.py
+++ cocoon/scheduler/models.py
@@ -5,14 +5,6 @@ from django.utils import timezone
 from cocoon.userAuth.models import MyUser
 from cocoon.houseDatabase.models import RentDatabaseModel
 
-
-class TimeModel(models.Model):
-    time = models.DateTimeField(default=timezone.now)
-
-    def __str__(self):
-        return self.time
-
-
 class ItineraryModel(models.Model):
     """
        Model for Itinerary. These are based on the interface designed on the Google Doc.
@@ -31,9 +23,6 @@ class ItineraryModel(models.Model):
     itinerary = models.FileField(blank=True)
     agent = models.ForeignKey(MyUser, related_name='scheduled_tours', on_delete=models.CASCADE, blank=True, null=True)
     tour_duration_seconds = models.IntegerField(default=0)
-    selected_start_time = models.OneToOneField(TimeModel, on_delete=models.CASCADE, blank=True, null=True)
-    available_start_times = models.ForeignKey(TimeModel, related_name='start_times', on_delete=models.CASCADE,
-                                              null=True, blank=True)
     homes = models.ManyToManyField(RentDatabaseModel, blank=True)
 
     def __str__(self):
@@ -53,3 +42,12 @@ class ItineraryModel(models.Model):
         :return:
         """
         return self.tour_duration_minutes/60
+
+
+class TimeModel(models.Model):
+    time = models.DateTimeField(default=timezone.now)
+    itinerary = models.ForeignKey(ItineraryModel, related_name='itinerary', on_delete=models.CASCADE)
+    selected_start_time = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.time
