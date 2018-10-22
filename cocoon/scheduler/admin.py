7b885be6a0cbec6be3c0c8036e94820b6aff8cbe
Sean Rayment <sean_rayment@brown.edu>
2018-10-22 10:44:14 -0400

    back to foreign key design

--- cocoon/scheduler/admin.py
+++ cocoon/scheduler/admin.py
@@ -4,16 +4,16 @@ from .models import ItineraryModel
 # Register your models here.
 
 
-class ItineraryAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('Client',
-         {'fields': ('client', 'homes')}
-         ),
-        ('Tour',
-         {'fields': ('itinerary', 'tour_duration_seconds', 'available_start_times')}),
-        ('Agent',
-         {'fields': ('agent', 'selected_start_time')})
-    ]
-
-
-admin.site.register(ItineraryModel, ItineraryAdmin)
+# class ItineraryAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         ('Client',
+#          {'fields': ('client', 'homes')}
+#          ),
+#         ('Tour',
+#          {'fields': ('itinerary', 'tour_duration_seconds', 'available_start_times')}),
+#         ('Agent',
+#          {'fields': ('agent', 'selected_start_time')})
+#     ]
+#
+#
+# admin.site.register(ItineraryModel, ItineraryAdmin)
