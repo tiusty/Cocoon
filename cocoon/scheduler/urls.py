f18701a6ebf3a4fd04e282fd49150297023abe2d
Sean Rayment <sean_rayment@brown.edu>
2018-10-27 18:37:14 -0400

    working on view for individual agent

--- cocoon/scheduler/urls.py
+++ cocoon/scheduler/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'scheduler'
 urlpatterns = [
     url(r'^agentScheduler/', views.agent_scheduler, name="agentScheduler"),
+    url(r'^myTours/', views.view_tours, name="myTours"),
 ]
--- cocoon/scheduler/views.py
+++ cocoon/scheduler/views.py
@@ -11,10 +11,30 @@ from cocoon.scheduler.models import ItineraryModel
 
 @login_required()
 def agent_scheduler(request):
+    context = {}
     current_profile = get_object_or_404(UserProfile, user=request.user)
     if current_profile.user.is_broker or current_profile.user.is_admin:
-        available_itineraries = ItineraryModel.objects.filter(agent=None)
-        context = {'itineraries': available_itineraries}
+        unclaimed_itineraries = ItineraryModel.objects.filter(agent=None)
+        claimed_itineraries = ItineraryModel.objects\
+            .filter(selected_start_time=None)\
+            .exclude(agent=None)
+        context['unclaimed_itineraries'] = unclaimed_itineraries
+        context['claimed_itineraries'] = claimed_itineraries
+
     else:
         raise Http404("This page does not exist")
     return render(request, 'scheduler/itineraryPicker.html', context)
+
+@login_required()
+def view_tours(request):
+    current_profile = get_object_or_404(UserProfile, user=request.user)
+    if current_profile.user.is_broker or current_profile.user.is_admin:
+        context = {}
+        unscheduled_itineraries = ItineraryModel.objects.filter(agent=current_profile.user, selected_start_time=None)
+        scheduled_itineraries = ItineraryModel.objects.fillter(agent=current_profile.user).exclude(selected_start_time=None)
+        context['unscheduled_itineraries'] = unscheduled_itineraries
+        context['scheduled_itineraries'] = scheduled_itineraries
+    else:
+        raise Http404("This page does not exist")
+    return render(request, 'scheduler/viewTours.html', context)
+
