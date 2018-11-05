d291fe66d43a8d13ce69bc5f738e3d8c22faa490
Nelson Cothran <nelsonc19@gmail.com>
2018-11-05 17:56:12 -0500

    Setup webpack to use with React

--- cocoon/frontend/src/pages/survey/index.js
+++ cocoon/frontend/src/pages/survey/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 
+import './survey.css';
+
 export default class Survey extends Component {
+    componentDidMount = () => {
+        console.log('rendered!');
+    }
+
     render(){
         return (
-            <h1>Rendering the survey here!</h1>
+            <h1 className="survey-headline">Survey will go here!</h1>
         );
     }
 }
---
+++ cocoon/static/js/base.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".message_timeout").fadeOut(1500);
+    },3000);
+});
