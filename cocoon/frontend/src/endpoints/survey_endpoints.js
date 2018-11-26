16abb5d3b8859383d1883f4d77068d1109edbe1a
Tiusty <awagud12@gmail.com>
2018-11-25 22:49:30 -0500

    [issue-221] Added another comment

--- cocoon/frontend/src/endpoints/survey_endpoints.js
+++ cocoon/frontend/src/endpoints/survey_endpoints.js
@@ -1,3 +1,9 @@
+/**
+ * Contains the URLS for the survey app
+ *
+ * The format/naming should match the urls.py for the survey app
+ * @type {{rentSurveyResult: string}}
+ */
 const survey_endpoints = {
     'rentSurveyResult': '/survey/rent/'
 };
--- cocoon/frontend/src/endpoints/userAuth_endpoints.js
+++ cocoon/frontend/src/endpoints/userAuth_endpoints.js
@@ -1,3 +1,9 @@
+/**
+ * Contains the URLS for the userAuth app
+ *
+ * The format/naming should match the urls.py for the userAuth app
+ * @type {{userSurveys: string}}
+ */
 const userAuth_endpoints = {
     'userSurveys': '/survey/api/userSurveys/'
 };
