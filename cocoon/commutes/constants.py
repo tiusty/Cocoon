52858a5642003153027782de05b32ff4dcc365f9
akashkumar2296 <akashkumar2296@gmail.com>
2018-12-10 21:34:05 -0500

    Made UNIX timestamps more human readable using datetime

--- cocoon/commutes/constants.py
+++ cocoon/commutes/constants.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import datetime
 import time
 
 # Controls how many days until the zip codes need to be refreshed
@@ -25,13 +26,23 @@ class CommuteAccuracy(Enum):
 # API does not accept times from the past
 # With traffic = Tuesday Dec 4, 2019 4:30pm
 # Without traffic = Tuesday Dec 4, 2019 3:30am
-COMMUTE_TIME_WITH_TRAFFIC = 1575477036
-COMMUTE_TIME_WITHOUT_TRAFFIC = 1575430236
-TRAFFIC_MODEL_DEFAULT = "best_guess"
 
+curr_year = 2019
+traffic_date = datetime.date(curr_year, 12, 4)
+with_traffic_time = datetime.time(16, 30) # 4:30pm
+without_traffic_time = datetime.time(3, 30) # 3:30am
+
+commute_time_traffic_temp = datetime.datetime.combine(traffic_date, with_traffic_time)
+commute_time_no_traffic_temp = datetime.datetime.combine(traffic_date, without_traffic_time)
 # Check if your commute time is in the future, if it is then change both times by a increasing it
 # a year, this makes sure that the time is always in the future
-if COMMUTE_TIME_WITH_TRAFFIC - time.time() <= 0 or COMMUTE_TIME_WITHOUT_TRAFFIC - time.time() <= 0:
-    COMMUTE_TIME_WITH_TRAFFIC += 31557600
-    COMMUTE_TIME_WITHOUT_TRAFFIC += 31557600
+
+if (commute_time_no_traffic_temp - datetime.datetime.now()).seconds <= 0 or \
+        (commute_time_traffic_temp - datetime.datetime.now()).seconds <= 0:
+    curr_year += 1
+    traffic_date = datetime.date(curr_year, 12, 4)
+
+COMMUTE_TIME_WITH_TRAFFIC = datetime.datetime.combine(traffic_date, with_traffic_time)
+COMMUTE_TIME_WITHOUT_TRAFFIC = datetime.datetime.combine(traffic_date, without_traffic_time)
+TRAFFIC_MODEL_DEFAULT = "best_guess"
 
