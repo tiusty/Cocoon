0c86cc1cf16d525f4c277e847f058503d4e76dd2
Tiusty <awagud12@gmail.com>
2018-12-15 19:46:21 -0500

    [issue-137] Created correct format of components

---
+++ cocoon/frontend/src/scheduler/clientScheduler/itineraryDaySelector.js
@@ -0,0 +1,15 @@
+// Import React Components
+import React from 'react'
+import { Component } from 'react';
+
+// Import Cocoon Components
+import ItineraryTimeRangeSelector from "./itineraryTimeRangeSelector";
+
+export default class ItineraryDaySelector extends Component {
+
+    render() {
+        return(
+            <ItineraryTimeRangeSelector/>
+        );
+    }
+}
--- cocoon/frontend/src/scheduler/clientScheduler/itineraryTimeSelector.js
+++ cocoon/frontend/src/scheduler/clientScheduler/itineraryTimeSelector.js
@@ -2,42 +2,14 @@
 import React from 'react'
 import { Component } from 'react';
 
-import TimeRange from 'react-time-range';
-import moment from 'moment';
+// Import Cocoon Components
+import ItineraryDaySelector from "./itineraryDaySelector";
 
-class ItineraryTimeSelector extends Component {
-    state = {
-        startTime: moment().startOf('day'),
-        endTime: moment().endOf('day').subtract(30, 'minutes'),
-    };
-
-    returnFunction = (time) => {
-        if (time.endTime) {
-            this.setState({
-                endTime: moment(time.endTime)
-            })
-        }
-        if (time.startTime) {
-            this.setState({
-                startTime: moment(time.startTime)
-            })
-        }
-    };
+export default class ItineraryTimeSelector extends Component {
 
     render() {
-        return (
-            <div>
-                <h2>The Time Selector</h2>
-                <TimeRange
-                    sameIsValid={false}
-                    startMoment={this.state.startTime}
-                    endMoment={this.state.endTime}
-                    onChange={this.returnFunction}
-                />
-            </div>
+        return(
+            <ItineraryDaySelector/>
         );
     }
-
 }
-
-export default ItineraryTimeSelector
