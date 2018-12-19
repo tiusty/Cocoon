f4dd787a8a3df7db05f62e8ed1d6d416912c2303
akashkumar2296 <akashkumar2296@gmail.com>
2018-12-18 20:27:27 -0500

    Test file added

---
+++ cocoon/homeDataNLPModel/tests_word_scraper.py
@@ -0,0 +1,12 @@
+from cocoon.homeDataNLPModel.word_scraper import WordScraper
+from django.test import TestCase
+
+class TestWordScraper(TestCase):
+
+    def setUp(self):
+        self.scraper = WordScraper()
+
+    def test_basic_word_search(self):
+        return False
+
+
