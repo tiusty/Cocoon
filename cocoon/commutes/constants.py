7f077869c0cdcb263b53b598bde59eecf2660861
Tiusty <awagud12@gmail.com>
2018-12-26 11:15:39 -0500

    [issue-181] Update the traffic time to 5:30pm

--- cocoon/commutes/constants.py
+++ cocoon/commutes/constants.py
@@ -20,12 +20,13 @@ class CommuteAccuracy(Enum):
     EXACT = 2
     DEFAULT = EXACT
 
-## This expression always results in a tuesday in the third week of january, on the next year from today
-d = datetime.date(datetime.datetime.today().year +1 , 1, 4)
-d = d + datetime.timedelta(weeks=(3-1), days=-d.weekday()+1)
 
-## 4:30pm for traffic
-## 3:30am for no-traffic
-COMMUTE_TIME_WITH_TRAFFIC = datetime.datetime.combine(d, datetime.time(16, 30)).timestamp()
+# This expression always results in a tuesday in the third week of january, on the next year from today
+d = datetime.date(datetime.datetime.today().year + 1 , 1, 4)
+d = d + datetime.timedelta(weeks=2, days=-d.weekday()+1)
+
+# 5:30pm for traffic
+# 3:30am for no-traffic
+COMMUTE_TIME_WITH_TRAFFIC = datetime.datetime.combine(d, datetime.time(17, 30)).timestamp()
 COMMUTE_TIME_WITHOUT_TRAFFIC = datetime.datetime.combine(d, datetime.time(3, 30)).timestamp()
 TRAFFIC_MODEL_DEFAULT = "best_guess"
