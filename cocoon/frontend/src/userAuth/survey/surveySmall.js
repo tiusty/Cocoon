fd255055d4c41860e1142cfbffd805cdb14bd5f3
Tiusty <awagud12@gmail.com>
2018-12-27 13:19:06 -0500

    [issue-150] Added load button

--- cocoon/frontend/src/userAuth/survey/surveySmall.js
+++ cocoon/frontend/src/userAuth/survey/surveySmall.js
@@ -6,19 +6,30 @@ import axios from 'axios'
 // Import Cocoon Components
 import './surveySmall.css'
 import surveyIcon from './Questionnaire_dragon-512.png';
+import survey_endpoints from '../../endpoints/survey_endpoints'
 
 export default class SurveySmall extends Component {
     state = {
         name: "",
         url: "",
     };
+
+    generateLoadUrl = () => {
+        /**
+         * Generates the URl so that the user can load a survey and it directs them to the survey results page for that
+         *  survey
+         */
+        return survey_endpoints['rentSurveyResult'] + this.state.url + "/";
+    };
+
     render() {
         return (
             <>
                 <div>
                     <div className="survey-small-box">
                         <img className="survey-small-icon" src={surveyIcon} alt="Survey icon"/>
-                        <button className="btn btn-primary survey-small-load-button">Load</button>
+                        {/*<button className="btn btn-primary survey-small-load-button">Load</button>*/}
+                        <a href={this.generateLoadUrl()} className="btn btn-primary survey-small-load-button">Load</a>
                         <p className="survey-small-title">Tyler, Alex, and Tomas</p>
                         <p className="survey-small-favorites">Number of favorites: 5</p>
                         <p className="survey-small-visit-list">Number of visit list: 3</p>
