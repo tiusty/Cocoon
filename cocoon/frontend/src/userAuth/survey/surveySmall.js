1622120fb32a3dbfa10a00a11e66e7b000ae1859
Tiusty <awagud12@gmail.com>
2018-12-27 13:12:45 -0500

    [issue-150] The basic layout is done now

--- cocoon/frontend/src/userAuth/survey/surveySmall.js
+++ cocoon/frontend/src/userAuth/survey/surveySmall.js
@@ -15,10 +15,14 @@ export default class SurveySmall extends Component {
     render() {
         return (
             <>
-                <div className="survey-div">
-                    <div className="survey-box">
-                        <img className="survey-icon" src={surveyIcon} alt="Survey icon"/>
-
+                <div>
+                    <div className="survey-small-box">
+                        <img className="survey-small-icon" src={surveyIcon} alt="Survey icon"/>
+                        <button className="btn btn-primary survey-small-load-button">Load</button>
+                        <p className="survey-small-title">Tyler, Alex, and Tomas</p>
+                        <p className="survey-small-favorites">Number of favorites: 5</p>
+                        <p className="survey-small-visit-list">Number of visit list: 3</p>
+                        <p className="survey-small-help-text">Click box to open</p>
                     </div>
                 </div>
             </>
