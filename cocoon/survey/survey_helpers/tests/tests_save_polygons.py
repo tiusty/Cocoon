70ea282f96e138e64972d3b6a3896318eb7a8bae
Tiusty <awagud12@gmail.com>
2019-01-15 18:28:50 -0500

    [issue-256] Adding even more unittests!

--- cocoon/survey/survey_helpers/tests/tests_save_polygons.py
+++ cocoon/survey/survey_helpers/tests/tests_save_polygons.py
@@ -39,7 +39,7 @@ class TestSavePolygons(TestCase):
     def test_valid_polygons_draw_on_map(self):
         """
         Tests that if the user choose to draw polygons on map and polygons were passed in the,
-            the polygons are saved to the survey
+            the polygons are saved to the survey. This tests one polygon was passed in
         """
         # Arrange
         filter_type = 1
@@ -60,3 +60,72 @@ class TestSavePolygons(TestCase):
         self.assertTrue(VertexModel.objects.filter(lat=Decimal('42.400677'), lng=Decimal('-71.127221')).exists())
         self.assertTrue(VertexModel.objects.filter(lat=Decimal('42.364159'), lng=Decimal('-70.976846')).exists())
         self.assertTrue(VertexModel.objects.filter(lat=Decimal('42.310357'), lng=Decimal('-71.110742')).exists())
+
+    def test_valid_polygons_2_draw_on_map(self):
+        """
+        Tests that if the user choose to draw polygons on map and polygons were passed in the,
+            the polygons are saved to the survey. This tests two polygons were passed in.
+        """
+        # Arrange
+        filter_type = 1
+        user = MyUser.objects.create(email="test@email.com")
+        survey = RentingSurveyModel.objects.create(user_profile=user.userProfile)
+        polygons = [
+            {'key': 1, 'vertices': [{'lat': 42.400677237104745, 'lng': -71.12722122802734},
+                                    {'lat': 42.36415890370297, 'lng': -70.9768458618164},
+                                    {'lat': 42.31035715086906, 'lng': -71.11074173583984}]},
+            {'key': 2, 'vertices': [{'lat': 43.400677237104745, 'lng': -69.12722122802734},
+                                    {'lat': 41.36415890370297, 'lng': -72.9768458618164},
+                                    {'lat': 39.31035715086906, 'lng': -70.11074173583984}]},
+        ]
+
+        # Act
+        save_polygons(survey, polygons, filter_type)
+
+        # Assert
+        self.assertEqual(PolygonModel.objects.count(), 2)
+        self.assertEqual(VertexModel.objects.count(), 6)
+
+        # Assert the vertices were created
+        self.assertTrue(VertexModel.objects.filter(lat=Decimal('42.400677'), lng=Decimal('-71.127221')).exists())
+        self.assertTrue(VertexModel.objects.filter(lat=Decimal('42.364159'), lng=Decimal('-70.976846')).exists())
+        self.assertTrue(VertexModel.objects.filter(lat=Decimal('42.310357'), lng=Decimal('-71.110742')).exists())
+
+        self.assertTrue(VertexModel.objects.filter(lat=Decimal('43.400677'), lng=Decimal('-69.127221')).exists())
+        self.assertTrue(VertexModel.objects.filter(lat=Decimal('41.364159'), lng=Decimal('-72.976846')).exists())
+        self.assertTrue(VertexModel.objects.filter(lat=Decimal('39.310357'), lng=Decimal('-70.110742')).exists())
+
+    def test_two_vertex_polygons_draw_on_map(self):
+        """
+        Tests that if a polygon with only two vertices are passed in, then the polygon is not saved
+        """
+        # Arrange
+        filter_type = 1
+        user = MyUser.objects.create(email="test@email.com")
+        survey = RentingSurveyModel.objects.create(user_profile=user.userProfile)
+        polygons = [{'key': 1, 'vertices': [{'lat': 42.400677237104745, 'lng': -71.12722122802734},
+                                            {'lat': 42.31035715086906, 'lng': -71.11074173583984}]}]
+
+        # Act
+        save_polygons(survey, polygons, filter_type)
+
+        # Assert
+        self.assertEqual(PolygonModel.objects.count(), 0)
+        self.assertEqual(VertexModel.objects.count(), 0)
+
+    def test_one_vertex_polygons_draw_on_map(self):
+        """
+        Tests that if a polygon with only one vertex is passed in then it is not saved
+        """
+        # Arrange
+        filter_type = 1
+        user = MyUser.objects.create(email="test@email.com")
+        survey = RentingSurveyModel.objects.create(user_profile=user.userProfile)
+        polygons = [{'key': 1, 'vertices': [{'lat': 42.400677237104745, 'lng': -71.12722122802734}]}]
+
+        # Act
+        save_polygons(survey, polygons, filter_type)
+
+        # Assert
+        self.assertEqual(PolygonModel.objects.count(), 0)
+        self.assertEqual(VertexModel.objects.count(), 0)
