8ebd0d48b847415f63111a54901fe04a5fd52a76
akashkumar2296 <akashkumar2296@gmail.com>
2019-01-18 19:31:02 -0500

    Commented, ready for PR

--- cocoon/houseDatabase/management/commands/helpers/word_scraper.py
+++ cocoon/houseDatabase/management/commands/helpers/word_scraper.py
@@ -7,6 +7,11 @@ class WordScraper:
         self.input_dictionary = self.make_input_dictionary()
 
     def make_input_dictionary(self):
+        """
+        Makes a dictionary using input description in lowercase, with "a/c" being replaced with "ac"
+        self.input_dictionary : {"word" : [list of indicies of occurance]}
+        uses regex expression to find all words and punctuations
+        """
         word_list = re.findall(r"[\w']+|[.,!?;]", self.input_description)
         word_dictionary = {}
         for i in range(len(word_list)):
@@ -18,6 +23,16 @@ class WordScraper:
         return word_dictionary
 
     def word_finder(self, word_list):
+        """
+        word_list: (list of strings) input word
+
+        Uses a list of words as input to find words in sequence using their index.
+        Case1: if list_index == 0, means that the word could not be found in the input description (FALSE)
+        Case2: if len(list_index)==1 and len(word_list) == 1, this means that there is only one word and it has been found (TRUE)
+        Case3: all the words in the word_list are present in the input description, and there are more than one. Use the first
+        word's indicies and check to see if each of the adjacent words match the index+1, +2, +3..... depending on the number
+        of words in the input.
+        """
 
         list_index = []
         for item in word_list:
