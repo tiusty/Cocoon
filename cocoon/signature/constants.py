3eeea28cc76e20d7e95e0cf759c1489248ace59e
Tiusty <awagud12@gmail.com>
2019-01-31 14:19:08 -0500

    [issue-293] Started conversion

--- cocoon/signature/constants.py
+++ cocoon/signature/constants.py
@@ -1,10 +1,16 @@
+from django.conf import settings
+
 # Docusign account variables
-INTEGRATOR_KEY = "37951692-e5fe-4e15-af79-9183ac019a57"
-BASE_URL = "https://demo.docusign.net/restapi"
-OAUTH_BASE_URL = "account-d.docusign.com" # use account.docusign.com for Live/Production
 REDIRECT_URI = "https://bostoncocoon.com"
-USER_ID = "4d882612-2587-4842-b32b-8d7e24458aba"
-ACCOUNT_ID = "6769317"
+
+# Docusign variables dependent on settings file
+if settings.DEBUG:
+    OAUTH_BASE_URL = "account-d.docusign.com" # use account.docusign.com for Live/Production
+    BASE_URL = "https://demo.docusign.net/restapi"
+    USER_ID = "4d882612-2587-4842-b32b-8d7e24458aba"
+    ACCOUNT_ID = "6769317"
+    INTEGRATOR_KEY = "37951692-e5fe-4e15-af79-9183ac019a57"
+
 
 # Template IDs
 PRE_TOUR_TEMPLATE_ID = 'e998b44f-28cb-4d20-ad67-97a033cbbab1'
