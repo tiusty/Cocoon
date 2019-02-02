7f1aa9438ea646a58e5ef4a7881ea023afcc7a9e
Nelson Cothran <nelsonc19@gmail.com>
2019-02-02 11:54:26 -0500

    moves navbar function to base main directory

--- cocoon/homePage/static/homePage/landingPage.js
+++ cocoon/homePage/static/homePage/landingPage.js
@@ -1,21 +1,3 @@
-function handleMenu() {
-  /**
-   * This function toggles the hamburger menu open/close
-   * Toggles class .mobile-links_open
-   **/
-  document.querySelector('.hamburger-menu').addEventListener('click', function() {
-    document.querySelector('.hamburger-menu').classList.toggle('mobile-links_open');
-    document.querySelector('.mobile-links').classList.toggle('mobile-links_open');
-  });
-
-  window.addEventListener('resize', function() {
-    if (window.innerWidth > 768) {
-      document.querySelector('.hamburger-menu').classList.remove('mobile-links_open');
-      document.querySelector('.mobile-links').classList.remove('mobile-links_open');
-    }
-  })
-}
-
 const Slider = (function() {
   const onboardingSlider = document.getElementById('onboarding');
   const landingPage = document.getElementById('landing-page-body');
--- cocoon/static/cocoon/main/js/navbar.js
+++ cocoon/static/cocoon/main/js/navbar.js
@@ -1,77 +1,19 @@
-/**
- * Created by srayment on 6/20/17.
- */
+function handleMenu() {
+  /**
+   * This function toggles the hamburger menu open/close
+   * Toggles class .mobile-links_open
+   **/
+  document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.hamburger-menu').classList.toggle('mobile-links_open');
+    document.querySelector('.mobile-links').classList.toggle('mobile-links_open');
+  });
 
-$(document).ready(function () {
-
-    if ($(window).scrollTop() < 60) {
-
-        $('.navbar').show();
-
-        $('.navbar').removeClass("navbar-animated");
-
-        $(".navbar-brand").removeClass("nav-brand-animated");
-        $(".nav > li > a").removeClass("link-animated");
-        $(".nav-logo").removeClass("nav-logo-animated");
-        $(".signup-button").removeClass("button-link-animated")
-        $(".register-link").removeClass("register-link-animated");
-    } else if ($(this).scrollTop() < 100) {
-        $('.navbar').hide();
-
-
-    } else {
-        $('.navbar').addClass('navbar-animated');
-
-        $(".navbar-brand").addClass("nav-brand-animated");
-        $(".nav > li > a").addClass("link-animated");
-        $(".nav-logo").addClass("nav-logo-animated");
-        $(".signup-button").addClass("button-link-animated")
-        $(".register-link").addClass("register-link-animated");
-        $('.navbar').show();
+  window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) {
+      document.querySelector('.hamburger-menu').classList.remove('mobile-links_open');
+      document.querySelector('.mobile-links').classList.remove('mobile-links_open');
     }
+  })
+}
 
-    $(window).scroll(function () {
-
-         $(".sidenav").css("top", $('.navbar').height() - $(this).scrollTop());
-
-        if ($(this).scrollTop() < 60) {
-
-            $('.navbar').show();
-
-            $('.navbar').removeClass("navbar-animated");
-
-            $(".navbar-brand").removeClass("nav-brand-animated");
-            $(".nav > li > a").removeClass("link-animated");
-            $(".nav-logo").removeClass("nav-logo-animated");
-            $(".signup-button").removeClass("button-link-animated")
-            $(".register-link").removeClass("register-link-animated");
-        } else if ($(this).scrollTop() < 100) {
-            $('.navbar').hide();
-
-
-        } else {
-            $('.navbar').show();
-            $('.navbar').addClass('navbar-animated');
-
-            $(".navbar-brand").addClass("nav-brand-animated");
-            $(".nav > li > a").addClass("link-animated");
-            $(".nav-logo").addClass("nav-logo-animated");
-            $(".signup-button").addClass("button-link-animated")
-            $(".register-link").addClass("register-link-animated");
-        }
-    });
-
-    $(window).resize(function () {
-        if ($(this).width() <= 768) {
-
-            // Also moves the sidenav down
-            $(".sidenav").removeClass("sidenav-down");
-
-            $(".navbar-brand").removeClass("nav-brand-animated");
-            $(".nav > li > a").removeClass("link-animated");
-            $(".nav-logo").removeClass("nav-logo-animated");
-            $(".signup-button").removeClass("button-link-animated")
-            $(".register-link").removeClass("register-link-animated");
-        }
-    })
-})
+window.onload = handleMenu;
