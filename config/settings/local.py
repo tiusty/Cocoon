22322bdccaf071824a2cee38b25e2115a807871e
Tiusty <awagud12@gmail.com>
2019-02-06 16:36:37 -0500

    [issue-132] Turn debug back on for local development

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Domains that are allowed
 ALLOWED_HOSTS = ['127.0.0.1', 'bostoncocoon.com', 'cocoonbeta.com', 'localhost']
