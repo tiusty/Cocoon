364f39acb04c26c65b6f3667cefd167b09bcddc9
Tiusty <awagud12@gmail.com>
2019-02-06 16:30:05 -0500

    [issue-132] Update class

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Domains that are allowed
 ALLOWED_HOSTS = ['127.0.0.1', 'bostoncocoon.com', 'cocoonbeta.com', 'localhost']
--- config/storage_backends.py
+++ config/storage_backends.py
@@ -1,13 +1,13 @@
 # Third party imports
 from storages.backends.s3boto3 import S3Boto3Storage
-from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 
 # Settings import
 from config.settings.production import AWS_STATIC_LOCATION, AWS_PUBLIC_MEDIA_LOCATION
 
 
 # Determines the storage location for static files
-class StaticStorage(ManifestStaticFilesStorage, S3Boto3Storage):
+class StaticStorage(ManifestFilesMixin, S3Boto3Storage):
     location = AWS_STATIC_LOCATION
 
 
