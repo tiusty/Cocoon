8eab6b60979d7b70265a07972891e7cbab4e1a56
Tiusty <awagud12@gmail.com>
2019-02-07 18:34:28 -0500

    [issue-318] Updated trackers model format

--- cocoon/dataAnalysis/models.py
+++ cocoon/dataAnalysis/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
 
-class SurveyResultsTracker(models.Model):
-    pass
+class Trackers(models.Model):
+    SURVEY_RESULT_TRACKER = 'sr'
+    TRACKER_TYPES = (
+        (SURVEY_RESULT_TRACKER, 'Survey Results Tracker')
+    )
+
+    tracker_type = models.CharField(
+        unique=True,
+        choices=TRACKER_TYPES,
+        max_length=2,
+    )
+
+    def get_survey_results_tracker(self):
+        (tracker, created) = Trackers.objects.get_or_create(tracker_type=self.SURVEY_RESULT_TRACKER)
+        return tracker
+
+    def __str__(self):
+        return self.get_tracker_type_display()
 
 
 class SurveyResultsIteration(models.Model):
-    tracker = models.ForeignKey(SurveyResultsTracker, related_name="iterations")
+    tracker = models.ForeignKey(Trackers, related_name="iterations")
 
 
 class HomeTracker(models.Model):
