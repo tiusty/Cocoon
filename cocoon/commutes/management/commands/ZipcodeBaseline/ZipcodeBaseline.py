067f33c0c8707659d721e3d9903f03482eee9b78
Tiusty <awagud12@gmail.com>
2019-02-11 16:36:41 -0500

    Moved files to correct location

--- cocoon/commutes/management/commands/ZipcodeBaseline/ZipcodeBaseline.py
+++ cocoon/commutes/management/commands/ZipcodeBaseline/ZipcodeBaseline.py
@@ -8,71 +8,40 @@ from googlemaps import distance_matrix, client
 # import API key from settings
 from config.settings.Global_Config import gmaps_api_key
 
-from django.core.management.base import BaseCommand
-
 # Retrieve Constants
 from cocoon.commutes.constants import GoogleCommuteNaming
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-class Command(BaseCommand):
 
-    def add_arguments(self, parser):
-        # Argument allows zipcodes to be pulled
-        parser.add_argument(
-            '--create_baseline',
-            action='store_true',
-            dest='create_baseline',
-            help='Creates baseline for zipcode destinations',
-        )
+class ZipcodeBaseline(object):
 
-        parser.add_argument(
-            '--commute_type',
-            type=str,
-            help='Specify a commute type',
-        )
-
-
-    def handle(self, *args, **options):
+    def create_baseline(self, commute_type):
         """
-        :param args:
-        :param options:
-        :return:
+        :param commute_type: (string) -> The commute type that the baseline is being made for
 
-        Function to run the command. list_zip_codes is all possible zip codes in Boston. The handler
-        calls the commute_approximation function, which writes the JSON file "approximations.txt"
+        Function to run the command. list_zip_codes is all possible zip codes in Boston
         """
         verify = raw_input("Please don't run this script unless you are directed to, please type 'yes' to confirm: ")
-        if verify.lower() != "yes" and verify.lower() != "y":
+        if verify.lower() != "yes":
             exit()
 
-        if options['create_baseline']:
-            commute_type = "driving"
-            if options['commute_type'] and (options['commute_type'].lower() == "driving" or \
-                                            options['commute_type'].lower() == "transit"):
+        list_zip_codes = []
 
-                commute_type = options['commute_type'].lower()
+        with open(BASE_DIR + "/ZipcodeBaseline/zip_codes_MA.txt", "r") as f:
+            for line in f:
+                line = line.split()
+                list_zip_codes.append(str(line[0]))
 
+        # Makes sures that all the zip-codes are distinct
+        list_zip_codes_distinct = list(set(list_zip_codes))
 
-            list_zip_codes = []
-            list_zip_codes_distinct = []
+        client_google = client.Client(gmaps_api_key)
 
-            with open(BASE_DIR + "/commands/zip_codes_MA.txt", "r") as f:
-                for line in f:
-                    line = line.split()
-                    list_zip_codes.append(str(line[0]))
+        self.commute_approximations(client_google, list_zip_codes_distinct, commute_type)
 
-                list_zip_codes_distinct = list(set(list_zip_codes))
-
-            units = "imerial"
-            client_google = client.Client(gmaps_api_key)
-
-            self.commute_approximations(units, client_google, list_zip_codes_distinct, commute_type)
-
-
-    def commute_approximations(self, units, client, list_zip_codes, commute_type):
+    def commute_approximations(self, client, list_zip_codes, commute_type):
         """
-        :param units: "imerial"
         :param client: google maps client
         :param list_zip_codes: list of zip codes in the Boston area
         :return:
@@ -99,7 +68,7 @@ class Command(BaseCommand):
                                                                 [i],
                                                                 [j],
                                                                 mode=commute_type_google,
-                                                                units=units)
+                                                                units="imperial")
 
                     json_commute["approximations"].append({
                         "origin":i,
@@ -107,6 +76,5 @@ class Command(BaseCommand):
                         "duration":response_json.get("rows")[0].get("elements")[0].get("duration").get("value"),
                         "distance":response_json.get("rows")[0].get("elements")[0].get("distance").get("value")
                     })
-
 
             f.write(json.dumps(json_commute,ensure_ascii=False))
