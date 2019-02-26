5d618395efa0cb8cd259c24d86ad9ad1ad73aa2a
Tiusty <awagud12@gmail.com>
2019-02-26 15:17:58 -0500

    [issue-370] Set the settings for the celery via the secret file

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery, shared_task
+from celery import Celery
+from config.settings.base import get_secret
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local_postgres')
+# set the default
+# Loads the settings file via the secrets file settings
+os.environ["DJANGO_SETTINGS_MODULE"] = get_secret('DJANGO_SETTINGS_MODULE')
 
 app = Celery('config')
 
