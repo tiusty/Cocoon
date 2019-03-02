afae2f4c491b6f182924803af50c26e665a2fcfd
seanrayment <rayment.code@gmail.com>
2019-03-02 14:32:43 +0100

    conitional rendering for survey amenities

--- cocoon/scheduler/templatetags/scheduler_extras.py
+++ cocoon/scheduler/templatetags/scheduler_extras.py
@@ -11,3 +11,10 @@ def try_string(string, alt_text):
     if string is "":
         return alt_text
     return string
+
+
+@register.simple_tag(name="condition")
+def condition(value, truthy, falsey):
+    if value:
+        return truthy
+    return falsey
