9c1b5e62419558c18fc91ac70e0d7f87185c0c75
Tiusty <awagud12@gmail.com>
2019-03-29 10:54:29 -0400

    Print places

--- cocoon/frontend/src/survey/resultsPage/map/map.js
+++ cocoon/frontend/src/survey/resultsPage/map/map.js
@@ -194,6 +194,7 @@ export default class Map extends Component {
 
 // Fit map to its bounds after the api is loaded
 const apiIsLoaded = (map, maps, places) => {
+    console.log(places)
   // Get bounds by our places
   const bounds = getMapBounds(map, maps, places);
   // Fit map to bounds
