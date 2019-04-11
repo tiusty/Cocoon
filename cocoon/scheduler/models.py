3a56ca54db70176ddaab053e1fabc684759d811b
Tiusty <awagud12@gmail.com>
2019-04-10 20:56:14 -0400

    [issue-459] Making the itinerary a foreign key since the survey can have multiple itineraries

--- cocoon/scheduler/models.py
+++ cocoon/scheduler/models.py
@@ -34,7 +34,7 @@ class ItineraryModel(models.Model):
            self.homes (ManytoManyField) -> Stores the homes that are associated with this itinerary
     """
     client = models.ForeignKey(MyUser, related_name='my_tours', on_delete=models.CASCADE)
-    survey = models.OneToOneField(RentingSurveyModel, related_name='itinerary', on_delete=models.SET_NULL, null=True)
+    survey = models.ForeignKey(RentingSurveyModel, related_name='itinerary', on_delete=models.SET_NULL, null=True)
     agent = models.ForeignKey(MyUser, related_name='scheduled_tours', on_delete=models.SET_NULL, blank=True, null=True)
     tour_duration_seconds = models.IntegerField(default=0)
     selected_start_time = models.DateTimeField(default=None, blank=True, null=True)
