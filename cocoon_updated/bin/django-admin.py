25c6cd726a9d06102ec1dbde69a09b1fdc494743
akashkumar2296 <akashkumar2296@gmail.com>
2019-04-17 22:08:49 -0400

    removed libraries

--- cocoon_updated/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kumarak/Desktop/cocoon_updated/Cocoon/cocoon_updated/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
